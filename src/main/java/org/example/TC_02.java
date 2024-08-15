ec2222c0c0e5f0fe555f9123df8e13b38422679b
Minu <testminu1111@gmail.com>
2024-08-15 22:42:21 +0530

    1st commit

---
+++ src/main/java/org/example/TC_02.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class TC_02
+
+{
+
+
+ @Test
+    void m1(){
+        System.out.println("Hii");
+    }
+}
---
+++ src/main/java/org/example/TC_1.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class TC_1 {
+
+    @Test
+    void  m1(){
+
+        System.out.println("Hello Java1");
+    }
+
+
+
+
+
+}
